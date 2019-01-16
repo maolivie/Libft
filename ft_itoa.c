ed03b557f509f232f36e36b8cc1796060a071e21
Marc OLIVIERI <maolivie@student.42.fr>
2019-01-16 15:20:22 +0100

    Shorter Itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,42 +6,43 @@
 /*   By: maolivie <maolivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/03 17:55:50 by maolivie          #+#    #+#             */
-/*   Updated: 2018/12/03 19:24:59 by maolivie         ###   ########.fr       */
+/*   Updated: 2019/01/16 15:18:04 by maolivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static size_t	nbr_len(int nbr)
+static unsigned short	nbr_len(int nbr)
 {
-	size_t			len;
 	unsigned int	abs;
+	unsigned short	len;
 
 	len = 1;
 	if (nbr < 0)
 	{
-		len++;
+		++len;
 		abs = -nbr;
 	}
 	else
 		abs = nbr;
-	while (abs > 9)
+	while (abs >= 10)
 	{
-		len++;
+		++len;
 		abs /= 10;
 	}
 	return (len);
 }
 
-char			*ft_itoa(int n)
+char					*ft_itoa(int n)
 {
 	char			*str;
-	size_t			len;
 	unsigned int	abs;
+	unsigned short	len;
 
 	len = nbr_len(n);
 	if (!(str = (char*)malloc(sizeof(char) * (len + 1))))
 		return (NULL);
+	str[len--] = '\0';
 	if (n < 0)
 	{
 		str[0] = '-';
@@ -49,13 +50,10 @@ char			*ft_itoa(int n)
 	}
 	else
 		abs = n;
-	str[len] = '\0';
-	len--;
-	while (abs > 9)
+	while (abs >= 10)
 	{
-		str[len] = (abs % 10) + '0';
+		str[len--] = (abs % 10) + '0';
 		abs /= 10;
-		len--;
 	}
 	str[len] = abs + '0';
 	return (str);
