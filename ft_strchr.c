6a05bab0be5b0c561f085d6fbf4ec482afd35cc8
Marc OLIVIERI <maolivie@student.42.fr>
2019-01-19 22:34:25 +0100

    Lighter strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: maolivie <maolivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/17 14:20:31 by maolivie          #+#    #+#             */
-/*   Updated: 2019/01/17 17:24:26 by maolivie         ###   ########.fr       */
+/*   Updated: 2019/01/19 22:28:34 by maolivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,5 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	size_t	i;
-	size_t	len;
-
-	i = 0;
-	len = ft_strlen(s);
-	while (i <= len)
-	{
-		if (s[i] == (char)c)
-			return ((char*)(s + i));
-		++i;
-	}
-	return (NULL);
+	return ((char*)ft_memchr((const void*)s, c, ft_strlen(s) + 1));
 }
