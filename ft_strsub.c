45bf692873d2350b658c16254b3865f3b8dbd164
Marc OLIVIERI <maolivie@student.42.fr>
2019-01-20 08:35:03 +0100

    Lighter strsub

--- ft_strsub.c
+++ ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: maolivie <maolivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/02 12:58:54 by maolivie          #+#    #+#             */
-/*   Updated: 2019/01/17 21:43:47 by maolivie         ###   ########.fr       */
+/*   Updated: 2019/01/20 08:22:02 by maolivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,5 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	if ((str = ft_strnew(len)) == NULL)
 		return (NULL);
-	while (len-- > 0)
-		str[len] = s[start + len];
-	return (str);
+	return (ft_strncpy(str, s + start, len));
 }
