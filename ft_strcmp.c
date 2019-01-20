73f882deb72f53cbc31ac8a2101e02b65c827250
Marc OLIVIERI <maolivie@student.42.fr>
2019-01-20 05:54:07 +0100

    Lighter strcmp

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: maolivie <maolivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/13 15:48:30 by maolivie          #+#    #+#             */
-/*   Updated: 2019/01/17 17:27:34 by maolivie         ###   ########.fr       */
+/*   Updated: 2019/01/20 05:51:04 by maolivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,5 @@
 
 int		ft_strcmp(const char *s1, const char *s2)
 {
-	size_t	i;
-
-	i = 0;
-	while (s1[i] && s1[i] == s2[i])
-		++i;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (ft_strncmp(s1, s2, ft_strlen(s1) + 1));
 }
