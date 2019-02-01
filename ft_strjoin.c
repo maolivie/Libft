1f300cc6f0c41b89fa301b9b247d957d9bb97d52
Marc OLIVIERI <maolivie@student.42.fr>
2019-02-01 23:17:27 +0100

    Better Strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: maolivie <maolivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/02 15:28:55 by maolivie          #+#    #+#             */
-/*   Updated: 2019/01/17 19:33:10 by maolivie         ###   ########.fr       */
+/*   Updated: 2019/02/01 23:12:39 by maolivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,11 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*str;
-	size_t	len_s1;
-	size_t	len_s2;
+	char *str;
 
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	len_s1 = ft_strlen(s1);
-	len_s2 = ft_strlen(s2);
-	if ((str = ft_strnew(len_s1 + len_s2)) == NULL)
+	if ((str = ft_strnew(ft_strlen(s1) + ft_strlen(s2))) == NULL)
 		return (NULL);
-	while (len_s2-- > 0)
-		str[len_s1 + len_s2] = s2[len_s2];
-	while (len_s1-- > 0)
-		str[len_s1] = s1[len_s1];
-	return (str);
+	return (ft_strcat(ft_strcpy(str, s1), s2));
 }
