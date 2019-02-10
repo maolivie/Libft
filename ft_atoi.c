32aa412e27bd854d6a721fb1c80eb261090ecb81
Marc OLIVIERI <maolivie@student.42.fr>
2019-02-10 19:37:45 +0100

    New atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: maolivie <maolivie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/30 11:30:10 by maolivie          #+#    #+#             */
-/*   Updated: 2019/01/17 17:17:37 by maolivie         ###   ########.fr       */
+/*   Updated: 2019/02/10 19:28:49 by maolivie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ int		ft_atoi(const char *str)
 		++str;
 	}
 	result = 0;
-	while (*str >= '0' && *str <= '9')
+	while (ft_isdigit(*str))
 		result = result * 10 + (*str++ - '0') * sign;
 	return (result);
 }
